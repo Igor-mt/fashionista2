5cff0b61de5a76b7a71cc750fa1e26f2c0900b39
Lucas Santana <lucas.diassantana@gmail.com>
2022-08-24 13:50:33 -0300

    criação do carrinho e home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React, { useState } from 'react'
 import { Home } from './pages/Home'
+import Navbar from './components/Navbar/Navbar'
+import ShoppingCart from './components/ShoppingCart/ShoppingCart'
+import Footer from './components/Footer/Footer'
+import { Overlay } from './GlobalStyles'
 
-const App = () => {  
+const App = () => {
+  const [isToggle, setToggle] = useState(false)
+  
   return (
-  <Home/>
+    <>
+      <Navbar>
+        <ShoppingCart
+          isToggle={isToggle}
+          setToggle={setToggle}
+        />
+      </Navbar>
+      <Home>{isToggle && <Overlay/>}</Home>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/GlobalStyles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 900;
+`;
