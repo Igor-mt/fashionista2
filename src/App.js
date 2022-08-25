d1c46cb7df33ae0abf78bec1d84ff32d1db656dd
Lucas Santana <lucas.diassantana@gmail.com>
2022-08-24 21:31:49 -0300

    Removed routes.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Feminino from "./pages/Feminino";
 import Home from "./pages/Home";
