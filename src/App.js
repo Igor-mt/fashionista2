ce09bc9261be400050382da55b924e631d72eaa8
Gustavo-Ae <gustavoaedias05@hotmail.com>
2022-08-26 14:34:14 -0300

    Tela Login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-
+import {BrowserRouter , Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Feminino from "./pages/Feminino";
 import Infantil from "./pages/Infantil";
 import Masculino from "./pages/Masculino";
+import TelaLogin from "./pages/TelaLogin";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/feminino/:id" element={<Feminino/>}></Route>
-      <Route path="/masculino/:id" element={<Masculino />}></Route>
-      <Route path="/infantil/:id" element={<Infantil />}></Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/feminino/:id" element={<Feminino/>}></Route>
+        <Route path="/masculino/:id" element={<Masculino />}></Route>
+        <Route path="/infantil/:id" element={<Infantil />}></Route>
+        <Route path="/login" element={<TelaLogin />}/>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/components/TelaLogin/Button/Button.js
@@ -0,0 +1,12 @@
+
+import "./Button.css";
+
+const Button = ({ type, children }) => {
+  return (
+    <button className="button" type={type}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
