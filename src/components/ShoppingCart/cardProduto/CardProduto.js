b03bbed6fe27f7059aaa1f81b8586db9d1957258
Lucas Santana <lucas.diassantana@gmail.com>
2022-08-27 22:54:52 -0300

    Melhoria do CardProduto do carrinho

--- src/components/ShoppingCart/cardProduto/CardProduto.js
+++ src/components/ShoppingCart/cardProduto/CardProduto.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import './CardProduto.css'
 
-const CardProduto = ({produto, onRemove}) => {
+const CardProduto = ({ produto, onRemove }) => {
     return (
         <>
             <div className="cart-produto-container">
                 <img className="cart-produto-image" src={produto.id.img} alt="" />
                 <div className="cart-produto-info">
                     <h1 className="cart-produto-title">{produto.id.name}</h1>
-                    <div className="cart-produto-quantity">Quantidade Total <div className="cart-produto-quantity-number">({produto.qtd})</div>R${produto.id.price}</div>
+                    <div className="cart-produto-quantity">Quantidade Total <div className="cart-produto-quantity-number">({produto.qtd})</div>
+                        <div className='cart-produto-price'>R${(produto.id.price).replace('.', ',')}</div>
+                    </div>
                 </div>
                 <button className="cart-produto-remove" onClick={() => onRemove(produto.id)}>✖</button>
             </div>
