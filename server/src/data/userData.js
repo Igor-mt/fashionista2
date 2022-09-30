03aed41c0226df67955daba33404115623e6d712
GustavoAe <gustavoaedias05@hotmail.com>
2022-09-30 16:40:03 -0300

    Criação do Sistema de Autenticação

--- server/src/data/userData.js
+++ server/src/data/userData.js
@@ -1,19 +1,19 @@
 const database = require('../infra/connection')
 
-exports.getExistentUser = (userData) => {
-    const {email, password} = userData
+exports.getExistentUser = (email) => {
 
     return database.oneOrNone(`
-    SELECT customer_id, email, "password"
-    FROM public.customers
-    WHERE email = '${email}' AND password = '${password}'
+        SELECT customer_id, email, "password"
+        FROM public.customers
+        WHERE email = '${email}' 
     `)
 }
 
-exports.createNewUser = (userData) => {
+exports.createNewUser = (userData, hashPassword) => {
     return database.query(`
     INSERT INTO public.customers
     (customer_id, "name", email, gender_id, cpf, birth, "password", cep, address, city, uf, district, address_number, phone)
-    VALUES(gen_random_uuid(), $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);
-    `, [userData.name, userData.email, userData.gender_id, userData.cpf, userData.birth, userData.password, userData.cep, userData.address, userData.city, userData.uf, userData.district, userData.address_number, userData.phone])
+    VALUES(gen_random_uuid(), $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+    RETURNING email, "password"
+    `, [userData.name, userData.email, userData.gender_id, userData.cpf, userData.birth, hashPassword, userData.cep, userData.address, userData.city, userData.uf, userData.district, userData.address_number, userData.phone])
 }
--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
 
 const productsRoutes = require('./routes/productsRoutes');
 const orderRoutes = require('./routes/ordersRoutes');
@@ -12,10 +13,19 @@ const port = process.env.PORT || 5450;
 
 let corsOptions = {
     origin: ['https://fashionista-hackadev.netlify.app', 'http://localhost:3000'],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200,
 }
 
+// app.use(function(req, res, next) {  
+//     res.header('Access-Control-Allow-Origin', req.headers.origin);
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+
 app.use(cors(corsOptions));
+app.use(cookieParser());
 app.use(express.json());
 
 // Rotas de Produtos
