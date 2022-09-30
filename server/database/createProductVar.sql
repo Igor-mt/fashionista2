31e6d4576361d0860ba6188be2b0959f6238212d
GustavoAe <gustavoaedias05@hotmail.com>
2022-09-29 23:49:40 -0300

    Colocando aspas no nome da coluna que está com palavra reservada do sql

--- server/database/createProductVar.sql
+++ server/database/createProductVar.sql
@@ -3,5 +3,5 @@ drop table if exists product_variation;
 create table product_variation (
 	product_variation_id uuid default gen_random_uuid() primary key not null,
 	product_id uuid not null,
-	size varchar not null
+	"size" varchar not null
 )
